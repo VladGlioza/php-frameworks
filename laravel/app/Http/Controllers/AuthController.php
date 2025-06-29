110662751e273c151218d4210c3ba8b9d064f9ff
Vlad Glioza <vt211_gvv@student.ztu.edu.ua>
2025-06-29 05:39:15 +0300

    lab05 done

---
+++ laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        if (!Auth::attempt($fields)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], 200);
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -8,15 +8,17 @@ use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\DiagnosisController;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\TreatmentController;
-
+use App\Http\Controllers\AuthController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('properties', PropertyController::class);
-Route::apiResource('patients', PatientController::class);
-Route::apiResource('doctors', DoctorController::class);
-Route::apiResource('diagnoses', DiagnosisController::class);
-Route::apiResource('appointments', AppointmentController::class);
-Route::apiResource('treatments', TreatmentController::class);
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::apiResource('patients', PatientController::class);
+    Route::apiResource('doctors', DoctorController::class);
+    Route::apiResource('appointments', AppointmentController::class);
+    Route::apiResource('diagnoses', DiagnosisController::class);
+    Route::apiResource('treatments', TreatmentController::class);
+});
+Route::post('/login', [AuthController::class, 'login']);
